0d3e8bb915a96ff1a0cff2ac3cef8432f625e5c1
bmancilla23 <bmancilla119@gmail.com>
2024-04-01 07:55:03 -0500

    Uso de BrowserAnimationsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+/* import { BrowserModule } from '@angular/platform-browser'; */
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    ToolbarComponent,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
